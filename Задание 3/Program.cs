396351bebb8bfb4ec806f61f37daf8c500717190
Gera <sergeev9927@gmail.com>
2023-11-20 23:14:37 +0300

    task3

--- Задание 3/Program.cs
+++ Задание 3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное целое число и на выходе показывает сумму первой и последней цифры этого числа.
+// 456  =>  10
+// 782  =>  9
+// 918  =>  17
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(System.Console.ReadLine());
+if (number > 99 && number < 999)
+{
+     int first = number / 100;
+    int last = number % 10;
+    int sum = first + last;
+Console.WriteLine("Сумма первой и последней цыфры равна:" +sum);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
